8b549f3a1329eb08bd8976e1a592e0cffcdda950
Sarah Eisa <sarah.ga.eisa@gmail.com>
2020-10-22 23:57:40 +0200

    Feat: Send a success message to parent thread

--- api/src/monochromeWorker.js
+++ api/src/monochromeWorker.js
@@ -5,4 +5,7 @@ gm(workerData.source)
     .monochrome()
     .write(workerData.destination, (error) => {
       if (error) throw error;
+      parentPort.postMessage({
+        monochrome: true,
+      });
     });
