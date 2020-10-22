28e3ffa76b7c91becb933955c46cc46319593b3e
Sarah Eisa <sarah.ga.eisa@gmail.com>
2020-10-22 15:38:10 +0200

    Feat: Configure multer disk storage

--- api/src/router.js
+++ api/src/router.js
@@ -1,6 +1,12 @@
 const { Router } = require('express');
+const multer = require('multer');
 
 const router = Router();
+
+const storage = multer.diskStorage({
+  destination: 'api/uploads/',
+  filename: filename,
+});
 
 function filename(request, file, callback) {
   callback(null, file.originalname);
