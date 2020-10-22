9a7ca39c8085071fa366aad57cd5b8d1bcb7b688
Sarah Eisa <sarah.ga.eisa@gmail.com>
2020-10-22 23:52:59 +0200

    Feat: Write the monochrome image to disk

--- api/src/monochromeWorker.js
+++ api/src/monochromeWorker.js
@@ -1,4 +1,4 @@
 const gm = require('gm');
 const { workerData, parentPort } = require('worker_threads');
 
-gm(workerData.source).monochrome();
+gm(workerData.source).monochrome().write(workerData.destination, () => {});
