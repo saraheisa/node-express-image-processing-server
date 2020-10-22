fe524a95e0b92d82b53ad897bb3f4f097a2313eb
Sarah Eisa <sarah.ga.eisa@gmail.com>
2020-10-23 01:33:18 +0200

    Feat: Define the source path

--- api/src/imageProcessor.js
+++ api/src/imageProcessor.js
@@ -4,7 +4,8 @@ const { Worker, isMainThread } = require('worker_threads');
 const pathToResizeWorker = path.resolve(__dirname, 'resizeWorker.js');
 const pathToMonochromeWorker = path.resolve(__dirname, 'monochromeWorker.js');
 
-function imageProcessor() {
+function imageProcessor(filename) {
+  const sourcePath  = uploadPathResolver(filename);
   return new Promise((resolve, reject) => {
     // check if we are in main thread
     if (isMainThread) {
