b09078aa9b2db5157c0923f46ce5db0163478824
Sarah Eisa <sarah.ga.eisa@gmail.com>
2020-10-22 23:38:03 +0200

    Feat: Handle resize errors gracefully

--- api/src/resizeWorker.js
+++ api/src/resizeWorker.js
@@ -1,4 +1,8 @@
 const gm = require('gm');
 const { workerData, parentPort } = require('worker_threads');
 
-gm(workerData.source).resize(100, 100).write(workerData.destination, ()=>{});
+gm(workerData.source)
+    .resize(100, 100)
+    .write(workerData.destination, (error) => {
+      if (error) throw error;
+    });
