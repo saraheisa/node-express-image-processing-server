aba8c480eb11a70ed5a9fbd4f4d1423263f66df9
Sarah Eisa <sarah.ga.eisa@gmail.com>
2020-10-23 01:15:27 +0200

    Feat: Resolve the path to the resize worker

--- api/src/imageProcessor.js
+++ api/src/imageProcessor.js
@@ -1,6 +1,8 @@
 const path = require('path');
 const { Worker, isMainThread } = require('worker_threads');
 
+const pathToResizeWorker = path.resolve(__dirname, 'resizeWorker.js');
+
 function imageProcessor() {
   return new Promise((resolve, reject) => {
     // check if we are in main thread
