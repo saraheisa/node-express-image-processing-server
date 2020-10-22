5723289b883e6ca26c2251b1fd5184a70c187320
Sarah Eisa <sarah.ga.eisa@gmail.com>
2020-10-23 01:11:49 +0200

    Feat: Instantiate a new promise

--- api/src/imageProcessor.js
+++ api/src/imageProcessor.js
@@ -1,6 +1,15 @@
 const path = require('path');
 const { Worker, isMainThread } = require('worker_threads');
 
-function imageProcessor() {}
+function imageProcessor() {
+  return new Promise((resolve, reject) => {
+    // check if we are in main thread
+    if (isMainThread) {
+      resolve();
+    } else {
+      reject(new Error('not on main thread'));
+    }
+  });
+}
 
 module.exports = imageProcessor;
