b242679c3ba2695e161a635f778cf8fe44420ef7
Sarah Eisa <sarah.ga.eisa@gmail.com>
2020-10-22 23:44:49 +0200

    Feat: Send a message to the parent thread

--- api/src/resizeWorker.js
+++ api/src/resizeWorker.js
@@ -5,4 +5,7 @@ gm(workerData.source)
     .resize(100, 100)
     .write(workerData.destination, (error) => {
       if (error) throw error;
+      parentPort.postMessage({
+        resized: true,
+      });
     });
