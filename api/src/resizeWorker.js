4fd0c623f250530027bcb5fb3a55d440a3a98543
Sarah Eisa <sarah.ga.eisa@gmail.com>
2020-10-22 23:33:45 +0200

    Feat: Write the resized image to disk

--- api/src/resizeWorker.js
+++ api/src/resizeWorker.js
@@ -1,4 +1,4 @@
 const gm = require('gm');
 const { workerData, parentPort } = require('worker_threads');
 
-gm(workerData.source).resize(100, 100);
+gm(workerData.source).resize(100, 100).write(workerData.destination, ()=>{});
