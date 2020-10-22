49b00f061311f00f86bd5177a4b5587880e788a1
Sarah Eisa <sarah.ga.eisa@gmail.com>
2020-10-22 19:38:19 +0200

    Feat: Create the MIME type file filter

--- api/src/router.js
+++ api/src/router.js
@@ -12,4 +12,13 @@ function filename(request, file, callback) {
   callback(null, file.originalname);
 }
 
+function fileFilter(request, file, callback) {
+  if (file.mimetype !== 'image/png') {
+    request.fileValidationError = 'Wrong file type';
+    callback(null, false, new Error('Wrong file type'));
+  } else {
+    callback(null, true);
+  }
+}
+
 module.exports = router;
